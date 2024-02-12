30c10a61ceec59af25e3c6700c1b412f8abfc097
typehoon <115880908+typehoon@users.noreply.github.com>
2024-02-12 21:32:26 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int M, F;
+	while (1) {
+		scanf("%d %d", &M, &F);
+		if (M == 0 && F == 0) {
+			break;
+		}
+		else {
+			printf("%d\n", M + F);
+		}
+	}
+	return 0;
+}
